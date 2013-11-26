7ae02667505aec3f5e0d0b50ca9a0f5fdf0e8222
Harrison Schaen <hschaen@gmail.com>
2013-11-25 16:54:57 -0800

    Update to main Index file

--- wp-content/themes/videozoom/videozoom/index.php
+++ wp-content/themes/videozoom/videozoom/index.php
@@ -8,21 +8,23 @@
         <div id="featuredContent">
         	<?php if (option::get('featured_enable') == 'on' && is_home() && $paged < 2) { get_template_part('wpzoom', 'slider'); } // Show the Featured Slider? ?>
 		    </div>
-		    <?php if (option::get('sidebar_home') == 'off') { echo "<div class=\"full\">"; } ?>
-        <?php if ($paged < 2) { ?>
-          <!-- SHOWS -->
-  		    <?php get_template_part('shows'); ?>
-          <!-- END SHOWS -->
-          <!-- START SIDEBAR -->
-          <?php if (option::get('sidebar_home') == 'off') { echo "</div>"; } ?>
-    		  <?php if (option::get('sidebar_home') == 'on') { ?>
-    		    <?php if ($paged < 2) { get_sidebar(); } else { get_sidebar("second"); } ?>
-    		  <?php } ?>
-		    <!-- END SIDEBAR -->
-        <?php } ?>
-  </section><!-- /featureContent -->
-  <section id="content" class="subContent">
-    <?php get_template_part('postScrollers'); ?>
-  </section>
+        <div class="showsWithSidebar">
+		      <?php if (option::get('sidebar_home') == 'off') { echo "<div class=\"full\">"; } ?>
+          <?php if ($paged < 2) { ?>
+            <!-- SHOWS -->
+    		    <?php get_template_part('shows'); ?>
+            <!-- END SHOWS -->
+            <!-- START SIDEBAR -->
+            <?php if (option::get('sidebar_home') == 'off') { echo "</div>"; } ?>
+        		<?php if (option::get('sidebar_home') == 'on') { ?>
+        		<?php if ($paged < 2) { get_sidebar(); } else { get_sidebar("second"); } ?>
+        		<?php } ?>
+        		<!-- END SIDEBAR -->
+          <?php } ?>
+        </div>
+   </section><!-- /featureContent -->
+   <section id="content" class="subContent">
+   	<?php get_template_part('postScrollers'); ?>
+   </section>
 </section><!-- /#main -->
 <?php get_footer(); ?>
